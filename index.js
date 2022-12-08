5ce5406b1b9e68ee06f9e5ef80196d9fc54ac032
vape8001 <peter.valencic@nielsen.com>
2022-12-08 12:40:45 +0100

    Dodana knjižnica za logiranje in pomožna knjižnica util

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-"use strict"
+"use strict";
+const FILENAME = "index.js";
+
+require("dotenv").config();
+const { log4js } = require("./lib/logger");
+const log = log4js.getLogger(FILENAME);
+const util = require("./lib/util");
+const http = require("http");
+const express = require("express");
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send(`Pozdravljen uporabnik. To sporočilo je iz strežnika`);
+});
+
+//ustvarimo strežnik, ki posluša na določenih vratih
+app.listen(process.env.SERVER_PORT, () => {
+  log.info(util.printBanner("APP Server"));
+  log.info(
+    `Zagnan ${new Date().toISOString}, Strežnik posluša na vratih ${
+      process.env.SERVER_PORT
+    }`
+  );
+});
+
