b7fc7798e081c2b73c70b4a5140d12e590a28b70
vape8001 <peter.valencic@nielsen.com>
2022-12-08 15:32:08 +0100

    Dodana knjižnica za poizvedbe

--- index.js
+++ index.js
@@ -5,13 +5,25 @@ require("dotenv").config();
 const { log4js } = require("./lib/logger");
 const log = log4js.getLogger(FILENAME);
 const util = require("./lib/util");
-const http = require("http");
+const bodyParser = require('body-parser');
 const express = require("express");
 const app = express();
+const dbUtil = require('./lib/dbutil');
+
+app.use(bodyParser.json());
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.get('/',(req,res) => {
     res.send(`Pozdravljen uporabnik. To sporočilo je iz strežnika`);
 });
+
+app.get('/podatki',dbUtil.vrniTrenutnePodatke); 
+
 
 //ustvarimo strežnik, ki posluša na določenih vratih
 app.listen(process.env.SERVER_PORT, () => {
